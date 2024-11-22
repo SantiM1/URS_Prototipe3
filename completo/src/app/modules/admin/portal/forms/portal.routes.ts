ecfc16e0be449567f02bcd98b2975b0e6cd5dd04
SantiM <83973548+SantiM1@users.noreply.github.com>
2024-11-22 13:06:09 -0500

    Auto-generated commit

--- completo/src/app/modules/admin/portal/forms/portal.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { FormularioURSComponent } from 'app/modules/admin/portal/forms/formulario-urs/formulario-urs.component';
-
-export default [
-    {
-        path: '',
-        component: FormularioURSComponent,
-    },
-] as Routes;
